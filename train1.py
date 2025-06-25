531583a46c8e322caa01abc1bbca14c4ae4caa06
vvvvv19 <118036228+vvvvv19@users.noreply.github.com>
2025-06-25 10:47:56 +0800

    Update and rename train.py to train1.py

--- train1.py
+++ train1.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # model.load('yolo11n.pt') # 加载预训练权重,改进或者做对比实验时候不建议打开，因为用预训练模型整体精度没有很明显的提升
-    model = YOLO('ultralytics/cfg/models/11/yolo11n4.yaml')  # 指定YOLO模型对象，并加载指定配置文件中的模型配置
+    model = YOLO('yolo11.yaml')  # 指定YOLO模型对象，并加载指定配置文件中的模型配置
     model.load('yolo11n.pt')      #加载预训练的权重文件，加速训练并提升模型性能
 
     model.train(data=r'/home/v/ultralytics-main/datasets/Overwatch Heads/data.yaml',
