8dd8de998c8c20ac2a9a4bcda7dae28b6be4f5c7
vvvvv19 <118036228+vvvvv19@users.noreply.github.com>
2025-06-25 23:52:42 +0800

    Update inf.py

--- inf.py
+++ inf.py
@@ -3,9 +3,16 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 
     # Load a model
+    model = YOLO(model=r'/home/v/ultralytics-main/result/exp/weights/best.pt')
+    model.predict(source=r'/home/v/ultralytics-main/datasets/Overwatch Heads/test/images/00059_png.rf.810b882ea37d9ebebef7159828819341.jpg',
+                  save=True,
+                  show=True,
+                  )
+    # Load a model
     model = YOLO(model=r'/home/v/ultralytics-main/result/exp2/weights/best.pt')
     model.predict(source=r'/home/v/ultralytics-main/datasets/Overwatch Heads/test/images/00059_png.rf.810b882ea37d9ebebef7159828819341.jpg',
                   save=True,
                   show=True,
                   )
+
 
